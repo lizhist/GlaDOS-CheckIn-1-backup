34854e1b3678230dc9844775ae51dd546c80f02e
QikaiXu <691924095@qq.com>
2021-01-23 23:36:26 +0800

    Update checkin.sh

--- checkin.sh
+++ checkin.sh
@@ -1,8 +1,5 @@
 echo '------------------sign------------------'
-checkin=`curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados_network"}' -X POST 'https://glados.rocks/api/user/checkin'`
-echo ''
-echo ${checkin} | grep -Eo '"message":"[^"]*"'
+curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados_network"}' -X POST 'https://glados.rocks/api/user/checkin' | grep -Eo '"message":"[^"]*"'
 echo '-----------------status-----------------'
-state=`curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status'`
-echo ${state} | grep -Eo '"leftDays":"[^"]*"'
+curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
