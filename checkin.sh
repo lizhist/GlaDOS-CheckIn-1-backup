be7ed13bd4a8a81e892abab65117358daa872e70
liyk1024 <35055407+liyk1024@users.noreply.github.com>
2023-02-07 14:33:15 +0800

    Update checkin.sh
    a

--- checkin.sh
+++ checkin.sh
@@ -3,5 +3,4 @@ curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '
 echo '-----------------status-----------------'
 leftDays=`curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"' |awk -F '"' '{print $4}' | awk -F '.' '{print $1}'`
 echo '-----------------Notify-----------------'
-key=`"${SCKEY}"`
-curl https://sctapi.ftqq.com/${key}.send?title=leftDays:${leftDays}
+curl https://sctapi.ftqq.com/SCT195207Te7mAlCUYmhRShdYIwDPLyTfA.send?title=leftDays:${leftDays}
